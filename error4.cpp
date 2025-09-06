9e65f8ebda32a3fe9891000e43624cfd65acb8f0
tg1kim <ktg@kangnam.ac.kr>
2025-09-07 04:59:49 +0900

    Add files via upload

---
+++ error4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#pragma warning(disable: )
+
+void main() 
+{
+	printf("마트에서 사올 품목");
+	printf("================");
+	printf("사과, 우유, 빵");
+	printf("================");
+}
---
+++ hello1.cpp
@@ -0,0 +1,8 @@
+include standard input ouput header file	//표준입출력
+
+#pragma warning(disable: )
+
+main 
+begin
+	Hello World!
+end
