6197d0a2a09fe2c2cac72677d70ff8e6e51aaa66
tg1kim <ktg@kangnam.ac.kr>
2025-09-12 17:24:59 +0900

    Add files via upload

---
+++ error3A.cpp
@@ -0,0 +1,10 @@
+/* 첫 번째 프로그램 */
+
+#include <stdio.h>
+
+#pragma warning(disable: 4996 4326 6031)
+
+void main() 
+{
+	print("Hello World!");
+}
---
+++ hello3A.cpp
@@ -0,0 +1,10 @@
+// ±èÃ¶¼ö
+#include <stdio.h>
+
+#pragma warning(disable: 4996 4326 6031)
+
+void main() 
+{
+	printf("Hello World!\n");
+	printf("Kim ChulSoo\n");
+}
